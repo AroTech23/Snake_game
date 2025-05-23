7f7ba9040e5b1106545246859a16423f1c13cfb5
AroTech <ngongarnold86@gmail.com>
2025-05-23 02:54:59 +0100

    just added the high score so it could update no matter what

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,8 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        with open ( "data.txt" ) as file:
+            content = int ( file.read () )
         self.score=0
         self.color ( "white" )
         self.penup()
